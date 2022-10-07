0c50911484c9a846e449b78afbf426f876ed5bc5
Ferdin Cardona <emanuelkar@hotmail.com>
2022-10-07 00:11:02 -0600

    se agregan getters y setters a pedido

--- tiendaConveniencia/src/main/java/com/grupo2/tiendaConveniencia/entity/Pedido.java
+++ tiendaConveniencia/src/main/java/com/grupo2/tiendaConveniencia/entity/Pedido.java
@@ -41,4 +41,62 @@ public class Pedido implements Serializable{
 	@Column(name = "STS_DESPACHO")
 	private Integer stsDespacho;
 
+	public Integer getIdTienda() {
+		return idTienda;
+	}
+
+	public void setIdTienda(Integer idTienda) {
+		this.idTienda = idTienda;
+	}
+
+	public Integer getuPadre() {
+		return uPadre;
+	}
+
+	public void setuPadre(Integer uPadre) {
+		this.uPadre = uPadre;
+	}
+
+	public Integer getIdUbicacion() {
+		return idUbicacion;
+	}
+
+	public void setIdUbicacion(Integer idUbicacion) {
+		this.idUbicacion = idUbicacion;
+	}
+
+	public Integer getIdVenta() {
+		return idVenta;
+	}
+
+	public void setIdVenta(Integer idVenta) {
+		this.idVenta = idVenta;
+	}
+
+	public String getIdicacionDespacho() {
+		return idicacionDespacho;
+	}
+
+	public void setIdicacionDespacho(String idicacionDespacho) {
+		this.idicacionDespacho = idicacionDespacho;
+	}
+
+	public String getObsDespacho() {
+		return obsDespacho;
+	}
+
+	public void setObsDespacho(String obsDespacho) {
+		this.obsDespacho = obsDespacho;
+	}
+
+	public Integer getStsDespacho() {
+		return stsDespacho;
+	}
+
+	public void setStsDespacho(Integer stsDespacho) {
+		this.stsDespacho = stsDespacho;
+	}
+	
+	
+
 }
